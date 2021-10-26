012f52220bda9708d61b899c4f7686692d2a860d
Reegan Mitchell <r.mitchell2@ncl.ac.uk>
2021-10-26 14:31:32 +0100

    Tutorial 2

---
+++ 2) Matrix Transformations/Renderer.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "../NCLGL/OGLRenderer.h"
+
+class Renderer : public OGLRenderer {
+public:
+	Renderer(Window& parent);
+	~Renderer(void);
+
+	void RenderScene()				override;
+
+	void SwitchToPerspective();
+	void SwitchToOrthographic();
+
+	inline void SetScale(float s) { scale = s; }
+	inline void SetRotation(float r) { rotation = r; }
+	inline void SetPosition(Vector3 p) { position = p; }
+protected:
+	Mesh* triangle;
+	Shader* matrixShader;
+	float scale;
+	float rotation;
+	Vector3 position;
+};
