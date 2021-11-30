62e66594e612bec8e7a19ba3593447b02ad82fd5
Reegan Mitchell <45948202+ReeganMitchell@users.noreply.github.com>
2021-11-30 00:14:59 +0000

    Restart Coursework

--- Blank Project/Renderer.cpp
+++ Blank Project/Renderer.cpp
@@ -1,30 +1,248 @@
 #include "Renderer.h"
+#include <nclgl/CubeRobot.h>
+#include <algorithm>
+#include <nclgl/MeshAnimation.h>
+#include <nclgl/MeshMaterial.h>
+#include <nclgl/Light.h>
+#include <nclgl/Camera.h>
 
-Renderer::Renderer(Window &parent) : OGLRenderer(parent)	{
-	triangle = Mesh::GenerateTriangle();
+const int POST_PASSES = 10;
+#define SHADOWSIZE 2048
 
-	basicShader = new Shader("basicVertex.glsl","colourFragment.glsl");
+Renderer::Renderer(Window& parent) : OGLRenderer(parent) {
+	camera = new  Camera(-30.0f, 315.0f, Vector3(-8.0f, 5.0f, 8.0f));
+	light = new  Light(Vector3(-20.0f, 10.0f, -20.0f),Vector4(1, 1, 1, 1), 250.0f);
 
-	if(!basicShader->LoadSuccess()) {
+	sceneShader = new Shader("ShadowSceneVertex.glsl", "ShadowSceneFragment.glsl");
+	shadowShader = new Shader("ShadowVertex.glsl", "ShadowFragment.glsl");
+	skyboxShader = new Shader("SkyboxVertex.glsl", "SkyboxFragment.glsl");
+	reflectShader = new Shader("ReflectVertex.glsl", "ReflectFragment.glsl");
+
+	if (!sceneShader->LoadSuccess() || !shadowShader->LoadSuccess() || !skyboxShader->LoadSuccess()) {
 		return;
 	}
 
+	glGenTextures(1, &shadowTex);
+	glBindTexture(GL_TEXTURE_2D, shadowTex);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOWSIZE, SHADOWSIZE, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	glGenFramebuffers(1, &shadowFBO);
+	glBindFramebuffer(GL_FRAMEBUFFER, shadowFBO);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, shadowTex, 0);
+	glDrawBuffer(GL_NONE);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	sceneMeshes.emplace_back(Mesh::GenerateQuad());
+	heightMap = new HeightMap(TEXTUREDIR"noise.png");
+	sceneMeshes.emplace_back(heightMap);
+	sceneMeshes.emplace_back(Mesh::LoadFromMeshFile("Sphere.msh"));
+	sceneMeshes.emplace_back(Mesh::LoadFromMeshFile("Cylinder.msh"));
+	sceneMeshes.emplace_back(Mesh::LoadFromMeshFile("Cone.msh"));
+
+	sceneDiffuse = SOIL_load_OGL_texture(TEXTUREDIR"Barren Reds.JPG", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS);
+	sceneBump = SOIL_load_OGL_texture(TEXTUREDIR"Barren RedsDOT3.JPG", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS);
+	cubeMap = SOIL_load_OGL_cubemap(TEXTUREDIR"rusted_west.jpg", TEXTUREDIR"rusted_east.jpg", TEXTUREDIR"rusted_up.jpg", TEXTUREDIR"rusted_down.jpg",
+		TEXTUREDIR"rusted_south.jpg", TEXTUREDIR"rusted_north.jpg", SOIL_LOAD_RGB, SOIL_CREATE_NEW_ID, 0);
+	waterTex = SOIL_load_OGL_texture(TEXTUREDIR"water.TGA", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS);
+
+	if (!sceneDiffuse || !sceneBump || !cubeMap || !waterTex) {
+		return;
+	}
+
+	SetTextureRepeating(sceneDiffuse, true);
+	SetTextureRepeating(sceneBump, true);
+	SetTextureRepeating(waterTex, true);
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	sceneTransforms.resize(5);
+	sceneTransforms[0] = Matrix4::Rotation(90, Vector3(1, 0, 0)) * Matrix4::Scale(Vector3(10, 10, 1));
+	sceneTransforms[1] = Matrix4::Translation(Vector3(0,-20,0)) * Matrix4::Scale(Vector3(0.1, 0.1, 0.1));
+	sceneTime = 0.0f;
+	waterRotate = 0.0f;
+	waterCycle = 0.0f;
 	init = true;
 }
-Renderer::~Renderer(void)	{
-	delete triangle;
-	delete basicShader;
+Renderer::~Renderer(void) {
+	glDeleteTextures(1, &shadowTex);
+	glDeleteFramebuffers(1, &shadowFBO);
+
+	for (auto& i : sceneMeshes) {
+		delete i;
+	}
+
+	delete shadowShader;
+	delete sceneShader;
+	delete camera;
+	delete light;
 }
 
-void Renderer::UpdateScene(float dt) {
+void  Renderer::UpdateScene(float dt) {
+	camera->UpdateCamera(dt);
+	sceneTime += dt;
 
+	frameFrustum.FromMatrix(projMatrix * viewMatrix);
+
+	for (int i = 2; i < 5; i++) {
+		Vector3 t = Vector3(-10 + (5 * i), 2.0f + sin(sceneTime * i), 0);
+		sceneTransforms[i] = Matrix4::Translation(t) * Matrix4::Rotation(sceneTime * 10 * i, Vector3(1, 0, 0));
+	}
 }
 
-void Renderer::RenderScene()	{
-	glClearColor(0.2f,0.2f,0.2f,1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);	
-
-	BindShader(basicShader);
-	triangle->Draw();
+void Renderer::PresentScene()
+{
+	
 }
 
+void Renderer::DrawPostProcess()
+{
+	
+}
+
+void Renderer::DrawMainScene()
+{
+	BindShader(sceneShader);
+	SetShaderLight(*light); 
+	viewMatrix = camera->BuildViewMatrix();
+	projMatrix = Matrix4::Perspective(1.0f, 15000.0f,(float)width / (float)height, 45.0f);
+
+	glUniform1i(glGetUniformLocation(sceneShader->GetProgram(), "diffuseTex"), 0); 
+	glUniform1i(glGetUniformLocation(sceneShader->GetProgram(), "bumpTex"), 1);
+	glUniform1i(glGetUniformLocation(sceneShader->GetProgram(), "shadowTex"), 2);
+	glUniform3fv(glGetUniformLocation(sceneShader->GetProgram(), "cameraPos"), 1, (float*)& camera->GetPosition());
+
+	glActiveTexture(GL_TEXTURE0); 
+	glBindTexture(GL_TEXTURE_2D, sceneDiffuse); 
+	glActiveTexture(GL_TEXTURE1); 
+	glBindTexture(GL_TEXTURE_2D, sceneBump); 
+	glActiveTexture(GL_TEXTURE2);
+	glBindTexture(GL_TEXTURE_2D, shadowTex); 
+
+	for (int i = 1; i < 5; ++i) { 
+		modelMatrix = sceneTransforms[i];
+		UpdateShaderMatrices();
+		sceneMeshes[i]->Draw(); 
+	}
+}
+
+void Renderer::DrawShadowScene()
+{
+	glBindFramebuffer(GL_FRAMEBUFFER, shadowFBO);
+
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glViewport(0, 0, SHADOWSIZE, SHADOWSIZE);
+	glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+
+	BindShader(shadowShader);
+
+	viewMatrix = Matrix4::BuildViewMatrix(light->GetPosition(), Vector3(0, 0, 0));
+	projMatrix = Matrix4::Perspective(1, 100, 1, 45);
+	shadowMatrix = projMatrix * viewMatrix; //used  later
+
+	for (int i = 1; i < 5; ++i) {
+		modelMatrix = sceneTransforms[i];
+		UpdateShaderMatrices();
+		sceneMeshes[i]->Draw();
+	}
+
+	glColorMask(GL_TRUE , GL_TRUE , GL_TRUE , GL_TRUE );
+	glViewport( 0, 0, width , height  );
+	glBindFramebuffer(GL_FRAMEBUFFER , 0);
+}
+
+void Renderer::DrawHeightMap()
+{
+	BindShader(sceneShader);
+	SetShaderLight(*light);
+	glUniform3fv(glGetUniformLocation(sceneShader->GetProgram(), "cameraPos"), 1, (float*)&camera->GetPosition());
+
+	glUniform1i(glGetUniformLocation(sceneShader->GetProgram(), "diffuseTex"), 0);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, sceneDiffuse);
+
+	glUniform1i(glGetUniformLocation(sceneShader->GetProgram(), "bumpTex"), 1);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, sceneBump);
+
+	glUniform1i(glGetUniformLocation(sceneShader->GetProgram(), "shadowTex"), 2);
+	glActiveTexture(GL_TEXTURE2);
+	glBindTexture(GL_TEXTURE_2D, shadowTex);
+
+	modelMatrix.ToIdentity();
+	textureMatrix.ToIdentity();
+
+	UpdateShaderMatrices();
+	SetShaderLight(*light);
+	heightMap->Draw();
+}
+
+void Renderer::DrawWater()
+{
+	BindShader(reflectShader);
+
+	glUniform3fv(glGetUniformLocation(reflectShader->GetProgram(), "cameraPos"), 1, (float*)&camera->GetPosition());
+
+	glUniform1i(glGetUniformLocation(reflectShader->GetProgram(), "diffuseTex"), 0);
+	glUniform1i(glGetUniformLocation(reflectShader->GetProgram(), "cubeTex"), 2);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, waterTex);
+
+	glActiveTexture(GL_TEXTURE2);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMap);
+
+	Vector3 hSize = heightMap->GetHeightMapSize();
+
+	modelMatrix = Matrix4::Translation(hSize * 0.5f) * Matrix4::Scale(hSize * 0.5f) * Matrix4::Rotation(90, Vector3(1, 0, 0));
+	textureMatrix = Matrix4::Translation(Vector3(waterCycle, 0.0f, waterCycle)) * Matrix4::Scale(Vector3(10, 10, 10)) * Matrix4::Rotation(waterRotate, Vector3(0, 0, 1));
+
+	UpdateShaderMatrices();
+	sceneMeshes[0]->Draw();
+}
+
+void Renderer::DrawSkybox()
+{
+	glDepthMask(GL_FALSE);
+
+	BindShader(skyboxShader);
+	UpdateShaderMatrices();
+
+	sceneMeshes[0]->Draw();
+
+	glDepthMask(GL_TRUE);
+}
+
+void Renderer::RenderScene() {
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+	DrawSkybox();
+	DrawShadowScene();
+	//DrawHeightMap();
+	DrawWater();
+	DrawMainScene();
+}
+
+void Renderer::BuildNodeLists(SceneNode* from)
+{
+	if (frameFrustum.InsideFrustum(*from)) {
+		Vector3 dir = from->GetWorldTransform().GetPositionVector() - camera->GetPosition();
+		from->SetCameraDistance(Vector3::Dot(dir, dir));
+
+		if (from->GetColour().w < 1.0f) {
+			transparentNodeList.push_back(from);
+		}
+		else {
+			nodeList.push_back(from);
+		}
+	}
+
+	for (vector<SceneNode*>::const_iterator i = from->GetChildIteratorStart(); i != from->GetChildIteratorEnd(); i++) {
+		BuildNodeLists((*i));
+	}
+}
--- Blank Project/Renderer.h
+++ Blank Project/Renderer.h
@@ -1,13 +1,73 @@
 #pragma once
 #include "../NCLGL/OGLRenderer.h"
+#include <nclgl\Camera.h>
+#include <nclgl\SceneNode.h>
+#include <nclgl\Frustum.h>
+#include <nclgl\HeightMap.h>
 
-class Renderer : public OGLRenderer	{
+class Camera;
+class SceneNode;
+class Mesh;
+class Shader;
+class HeightMap;
+class MeshMaterial;
+class MeshAnimation;
+class Light;
+
+class Renderer : public OGLRenderer {
 public:
-	Renderer(Window &parent);
-	 ~Renderer(void);
-	 void RenderScene()				override;
-	 void UpdateScene(float msec)	override;
+	Renderer(Window& parent);
+	~Renderer(void);
+
+	void RenderScene() override;
+	void UpdateScene(float dt) override;
 protected:
-	Mesh*	triangle;
-	Shader* basicShader;
+	void PresentScene();
+	void DrawPostProcess();
+	void DrawMainScene();
+	void DrawShadowScene();
+
+	void DrawHeightMap();
+	void DrawWater();
+	void DrawSkybox();
+
+	//Scene management stuff
+	void BuildNodeLists(SceneNode* from);
+	void SortNodeLists();
+	void ClearNodeLists();
+
+	vector<SceneNode*> transparentNodeList;
+	vector<SceneNode*> nodeList;
+	SceneNode* root;
+
+	//heightmap stuff
+	HeightMap* heightMap;
+
+	//skybox stuff
+	Shader* skyboxShader;
+	GLuint cubeMap;
+
+	//water stuff
+	GLuint waterTex;
+	Shader* reflectShader;
+	float waterRotate;
+	float waterCycle;
+
+	Shader* sceneShader;
+	Shader* shadowShader;
+
+	GLuint shadowTex;
+
+	GLuint sceneDiffuse;
+	GLuint sceneBump;
+	float sceneTime;
+
+	GLuint shadowFBO;
+
+	Light* light;
+	Camera* camera;
+	Frustum frameFrustum;
+
+	vector<Mesh*> sceneMeshes;
+	vector<Matrix4> sceneTransforms;
 };
